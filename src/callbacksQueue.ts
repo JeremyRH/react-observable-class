31629adbf8760cd799498ce2ed372f493c165cdd
JeremyRH <holcomb227@gmail.com>
2023-02-20 15:39:55 -0800

    fix!: flatten callacks queue to prevent mutations from adding callbacks

--- src/callbacksQueue.ts
+++ src/callbacksQueue.ts
@@ -1,7 +1,7 @@
 export type CB = () => any;
 
 const promise = Promise.resolve();
-const callacksQueue = new Set<Set<CB>>();
+const callacksQueue = new Set<CB>();
 const calledCallbacks = new Set<CB>();
 
 function callCallback(callback: CB) {
@@ -18,13 +18,11 @@ function callCallback(callback: CB) {
 
 export function scheduleCallbacks(callbackSet: Set<CB>) {
   const noPending = callacksQueue.size === 0;
-  callacksQueue.add(callbackSet);
+  callbackSet.forEach((callback) => callacksQueue.add(callback));
 
-  if (noPending) {
+  if (noPending && callbackSet.size > 0) {
     promise.then(() => {
-      callacksQueue.forEach((cbSet) => {
-        cbSet.forEach(callCallback);
-      });
+      callacksQueue.forEach(callCallback);
       calledCallbacks.clear();
       callacksQueue.clear();
     });
